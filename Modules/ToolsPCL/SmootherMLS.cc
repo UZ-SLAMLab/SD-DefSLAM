94559d9b56a9fa7b2f6d6b57ce6c04bffe7de3e9
jlamarcp <jose.lamarca.peiro@gmail.com>
2020-09-04 09:25:27 +0200

    fast commit

--- Modules/ToolsPCL/SmootherMLS.cc
+++ Modules/ToolsPCL/SmootherMLS.cc
@@ -22,7 +22,6 @@
 #include <SmootherMLS.h>
 
 #include <pcl/point_types.h>
-#include <pcl/kdtree/kdtree_flann.h>
 #include <pcl/surface/mls.h>
 #include <pcl/common/distances.h>
 
@@ -35,47 +34,6 @@ SmootherMLS::SmootherMLS(int polynomialOrder_, double searchRadius_)
                                           };
 
 using namespace pcl;
-
-// Constructor.
-std::vector<int> SmootherMLS::smoothPointCloud(const SmootherMLS::pointcloud &pcVector)
-{
-    pcl::PointCloud<PointXYZ>::Ptr cloud(new pcl::PointCloud<PointXYZ>());
-
-    for (uint i(0); i < pcVector.size(); i++)
-    {
-        PointXYZ pclpoint;
-        pclpoint.x = pcVector[i][0];
-        pclpoint.y = pcVector[i][1];
-        pclpoint.z = pcVector[i][2];
-        cloud->push_back(pclpoint);
-    }
-
-    pcl::search::KdTree<pcl::PointXYZ>::Ptr tree(new pcl::search::KdTree<pcl::PointXYZ>);
-    pcl::PointCloud<pcl::PointNormal> mls_points;
-    // Set parameters
-    pcl::MovingLeastSquares<pcl::PointXYZ, pcl::PointNormal> mls;
-    mls.setInputCloud(cloud);
-    mls.setComputeNormals(true); // To have the corresponding Indices
-    mls.setPolynomialOrder(polynomialOrder_);
-    mls.setSearchMethod(tree);
-    mls.setSearchRadius(searchRadius_);
-    mls.process(mls_points);
-    auto indices = (*(mls.getCorrespondingIndices())).indices;
-    SmootherMLS::pointcloud pcVectorO;
-    pcVectorO.reserve(mls_points.size());
-
-    for (uint i(0); i < pcVector.size(); i++)
-    {
-        std::vector<float> pt;
-        pt.reserve(3);
-        pt.push_back(mls_points[i].x);
-        pt.push_back(mls_points[i].y);
-        pt.push_back(mls_points[i].z);
-        pcVectorO.push_back(std::move(pt));
-    }
-
-    return indices;
-}
 
 std::vector<int> SmootherMLS::outlierRemovalRadius(const SmootherMLS::pointcloud &pcVector)
 {
